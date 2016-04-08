b7adcd44b294284aea95cc8799faea460badd511
cokelly <okellyconor@gmail.com>
2016-04-08 15:10:21 +0100

    Created main project files and added outline files

---
+++ Data_phraser.py
@@ -0,0 +1,11 @@
+#Author - Conor O'Kelly
+# The main code to take in and prase the text files
+
+from bs4 import BeautifulSoup
+
+def clean_html(file_name):
+
+
+
+if __name__ == '__main__':
+    print("Start")
