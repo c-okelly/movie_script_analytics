494c3989a8943128981c81c11bfab305e10af7f5
cokelly <okellyconor@gmail.com>
2016-04-13 17:52:19 +0100

    Created files for new functionality

---
+++ information_apis.py
@@ -0,0 +1,5 @@
+# Author Conor O'Kelly
+
+"""
+This file will contain all the functions that are required to fetch the extra data stored on external websites.
+"""
---
+++ script_object.py
@@ -0,0 +1,7 @@
+# Author Conor O'Kelly
+
+"""
+This file will contain the script object. It will take the input of a string object and the name of the script.
+Each object will the run functions of the script to generate data from it.
+This results can then be called by attributes to be put into a csv document
+"""
