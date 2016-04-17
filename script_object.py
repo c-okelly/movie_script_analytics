ebac5db089526fd0e54886661c7da9ad55e5d300
cokelly <okellyconor@gmail.com>
2016-04-17 13:35:49 +0100

    Created start of script object class

--- script_object.py
+++ script_object.py
@@ -5,3 +5,16 @@ This file will contain the script object. It will take the input of a string obj
 Each object will the run functions of the script to generate data from it.
 This results can then be called by attributes to be put into a csv document
 """
+
+class Script:
+
+    def __init__(self,script,movie_title,script_imdb_id,infomation_dict):
+        self.script = script
+        self.movie_title = movie_title
+        self.movei_imdb_id = script_imdb_id
+        self.information_dict = infomation_dict
+        self.script_dict = self.create_script_info_dict()
+
+    def create_script_info_dict(self,):
+
+        return 1
