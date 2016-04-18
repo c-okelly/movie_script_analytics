b173d39bdd12aee2dceb8ab69465bbb29b7f10bf
cokelly <okellyconor@gmail.com>
2016-04-18 15:40:36 +0100

    Updated tests

--- tests.py
+++ tests.py
@@ -2,38 +2,53 @@ import re
 
 f = open("Data/scripts_text/17-Again.txt", 'r')
 text = f.read()
-text = text[1000:1800]
-print(text)
+# text = text[900:1500]
+# print(text)
+
+# count = len(re.findall("\W+",text))
+# print(count)
 
 lines = text.split('\n')
-print(lines)
+lines_on_empty = re.split("\n\s+\n", text)
+print(len(lines))
+print(len(lines_on_empty))
 
-count = 0
+# Find empty lines
+# count = 0
 # for item in lines:
 #     if re.search("\A\s+\Z", item):
 #         print(count)
 #     count += 1
 
+# Search for character names in list
 # for item in lines:
-#     if re.search("\A\s*MAN\s*(\(.*\))?\s*\Z", item):
+#     if re.search("\A\s*Name_character\s*(\(.*\))?\s*\Z", item):
 #         print(item)
-characters = dict()
+#
+# # Generate list of characters from the script
+# characters = dict()
+#
+#
+# for line in lines:
+#     #Strip whitespace and check if whole line is in capital letters
+#     line = line.strip()
+#     if (line.isupper()):
+#
+#         # Exclude lines with EXT / INT in them
+#         s1 = re.search('EXT\.', line)
+#         s2 = re.search('INT\.', line)
+#
+#         # Select correct lines and strip out and elements within parathenses. Normally continued
+#         if (not(s1 or s2)):
+#             line = re.sub("\s*\(.*\)","",line)
+#             # If character no in dict add them. If a already in increase count by 1
+#             if line in characters:
+#                 characters[line] = characters[line] + 1
+#             else:
+#                 characters[line] = 1
+#
+# print(characters)
 
-for line in lines:
-    line = line.strip()
-    if (line.isupper()):
 
-        s1 = re.search('EXT\.', line)
-        s2 = re.search('INT\.', line)
 
-        if (not(s1 or s2)):
-            print(line)
-            re.sub("C","CC",line)
-            print(line)
-            print("One line")
-            if line in characters:
-                characters[line] = characters[line] + 1
-            else:
-                characters[line] = 1
-
-print(characters)
+# Get description lines
