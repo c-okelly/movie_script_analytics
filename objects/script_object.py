c7624a30b6769a482229b2b2b454e9a689e9c74e
cokelly <okellyconor@gmail.com>
2016-04-19 22:03:07 +0100

    Started work on main object class and script constructor method

--- objects/script_object.py
+++ objects/script_object.py
@@ -6,6 +6,9 @@ Each object will the run functions of the script to generate data from it.
 This results can then be called by attributes to be put into a csv document
 """
 
+from text_objects import Speech,Scene_change,Discription
+from information_apis import imdb_data_call
+
 class Script:
 
     def __init__(self,script,movie_title,script_imdb_id,infomation_dict):
@@ -15,6 +18,31 @@ class Script:
         self.information_dict = infomation_dict
         self.script_dict = self.create_script_info_dict()
 
-    def create_script_info_dict(self,):
+    # Match each of the sections depending on
+    def create_script_info_dict(self):
+
 
         return 1
+
+def create_script_object_from_name_and_file(script_text_file,file_name):
+
+    # Clean file name
+    cleand_name = file_name.replace("-", " ").replace(".txt", "")
+
+    # If last word in file is the move it to front
+    test_name = cleand_name.split()[-1].lower()
+    if (test_name == "the"):
+        cleand_name = cleand_name.replace(", The","").replace(", the","")
+        cleand_name = "The " + cleand_name
+
+    # Use imdb call to serach for meta date based on movie name
+    meta_data = imdb_data_call(cleand_name)
+
+
+    print(meta_data)
+
+if __name__ == '__main__':
+    with open("../Data/scripts_text/Abyss,-The.txt") as file:
+        text_file = file.read()
+
+    create_script_object_from_name_and_file(text_file,"Abyss,-The.txt")
--- objects/text_objects.py
+++ objects/text_objects.py
@@ -1,1 +1,24 @@
 # Author Conor O'Kelly
+
+class Discription:
+
+    def __init__(self,text,count):
+
+        self.text = text
+        self.count = count
+
+class Speech:
+
+    def __init__(self,text,count):
+
+        self.text = text
+        self.count = count
+
+class Scene_change:
+
+    def __init__(self,text,count):
+
+        self.text = text
+        self.count = text
+
+
