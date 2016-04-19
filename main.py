14b6c2a7074ecc555a707486d3889e1f27b5b09a
cokelly <okellyconor@gmail.com>
2016-04-19 22:02:32 +0100

    Started work on mian object constructor

---
+++ main.py
@@ -0,0 +1,22 @@
+# Author Conor O'Kelly
+
+"""
+This is main file used in the preperation of data
+"""
+
+def main():
+
+    # Call function to turn raw html files into text files
+
+    # Call each text file in turn and return a text object which is added to a array
+    script_object = []
+
+
+
+
+    print("Finish")
+
+
+
+if __name__ == '__main__':
+    main()
