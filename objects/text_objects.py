6770678cc3511a1caf58050a85f6bb961252d446
cokelly <okellyconor@gmail.com>
2016-04-20 12:09:14 +0100

    More updates to speech object

--- objects/text_objects.py
+++ objects/text_objects.py
@@ -3,6 +3,7 @@
 # from vaderSentiment.vaderSentiment import sentiment as VS
 from nltk import tokenize
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import re
 
 class Discription:
 
@@ -17,7 +18,19 @@ class Speech:
 
         self.text = text
         self.count = count
+        self.character = self.return_character_name()
         self.sentimnet = self.sentiment_analytis_text()
+        self.no_words = self.return_no_words()
+
+    def return_character_name(self):
+
+        text = self.text
+
+        character_name = text.split("\n")[0]
+
+        character_name = character_name.strip()
+
+        return character_name
 
     def sentiment_analytis_text(self):
 
@@ -26,10 +39,21 @@ class Speech:
         token_text = tokenize.sent_tokenize(text)
         sid = SentimentIntensityAnalyzer()
 
+        over_all_sentimnet = 0
         for sentence in token_text:
-            print(sentence)
+            # print(sentence)
             score = sid.polarity_scores(sentence)
-            print(score)
+            # print(score)
+            over_all_sentimnet += score.get("compound")
+        # print(over_all_sentimnet/10)
+
+    def return_no_words(self):
+
+        text = self.text
+        count = len(re.findall("\w+",text))
+
+        return count
+
 
 class Scene_change:
 
@@ -43,4 +67,10 @@ class Scene_change:
 
 if __name__ == '__main__':
 
-    text_ob = Speech("Congratulations, Mike. You deserve it. You're like a totally amazing salesman. You are terribly at your job",0.5)
+    text_ob = Speech("""                              WENDY
+                    Congratulations, Mike. You deserve
+                    it. You're like a totally amazing
+                    salesman.
+                        """,0.5)
+
+    print(text_ob.no_words)
