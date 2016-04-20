b867470cf5f023772472f19c2690b1bce7cd4106
cokelly <okellyconor@gmail.com>
2016-04-20 11:49:10 +0100

    Started sentiment analysis for speed text

--- objects/text_objects.py
+++ objects/text_objects.py
@@ -1,5 +1,9 @@
 # Author Conor O'Kelly
 
+# from vaderSentiment.vaderSentiment import sentiment as VS
+from nltk import tokenize
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
 class Discription:
 
     def __init__(self,text,count):
@@ -13,6 +17,19 @@ class Speech:
 
         self.text = text
         self.count = count
+        self.sentimnet = self.sentiment_analytis_text()
+
+    def sentiment_analytis_text(self):
+
+        text = self.text
+
+        token_text = tokenize.sent_tokenize(text)
+        sid = SentimentIntensityAnalyzer()
+
+        for sentence in token_text:
+            print(sentence)
+            score = sid.polarity_scores(sentence)
+            print(score)
 
 class Scene_change:
 
@@ -22,3 +39,8 @@ class Scene_change:
         self.count = text
 
 
+
+
+if __name__ == '__main__':
+
+    text_ob = Speech("Congratulations, Mike. You deserve it. You're like a totally amazing salesman. You are terribly at your job",0.5)
