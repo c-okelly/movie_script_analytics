dae1d4b8a4e708c70a0e75a9d81632775ebf2bee
cokelly <okellyconor@gmail.com>
2016-04-21 11:12:33 +0100

    Script object has basic attributed and clean name function working. Also calls api correctly

--- objects/script_object.py
+++ objects/script_object.py
@@ -9,43 +9,71 @@ This results can then be called by attributes to be put into a csv document
 from text_objects import Speech,Scene_change,Discription
 from information_apis import imdb_data_call
 
+class MoveDataNotFound(Exception):
+
+    def __init__(self,movie_name):
+        self.move_name = movie_name
+    def __repr__(self):
+        return "Failed to find OMDBAPI date for the movie " + self.move_name
+
 class Script:
 
-    def __init__(self,script,movie_title,script_imdb_id,infomation_dict):
-        self.script = script
-        self.movie_title = movie_title
-        self.movei_imdb_id = script_imdb_id
-        self.information_dict = infomation_dict
-        self.script_dict = self.create_script_info_dict()
+    def __init__(self,script_string,movie_file_name):
+        # Set attributed from inputs. One script sting and assoicated file name
+        self.script = script_string
+        # Reformat movie file name
+        self.movie_title = self.__return_cleaned_name(movie_file_name)
 
-    # Match each of the sections depending on
-    def create_script_info_dict(self):
+        # Attempt to fetch omdbapi data using info api
+        # try:
+        #     self.imdb_dict = imdb_data_call(self.movie_title)
+        # except:
+        #     raise MoveDataNotFound(movie_file_name)
+
+        # Created script info dict
+        self.script_info_dict = {}
+
+        # Create arrays to hold different script object
+        self.speech_object_array = []
+        self.__description_object_array = []
+        self.__scene_object_array = []
+
+        # Called array builder functions
+        self.__create_object_arrays_from_script()
+
+
+    def __repr__(self):
+        return "Moive script object of the file " + self.movie_title
+
+
+    def __return_cleaned_name(self,dirty_file_name):
+
+        # Clean file name
+        cleand_name = dirty_file_name.replace("-", " ").replace(".txt", "")
+
+        # If last word in file is 'the' move it to front
+        test_name = cleand_name.split()[-1].lower()
+        if (test_name == "the"):
+            cleand_name = cleand_name.replace(", The","").replace(", the","")
+            cleaned_file_name = "The " + cleand_name
+        else:
+            cleaned_file_name = cleand_name
+
+        return cleaned_file_name
+
+
+    # Text text and sort
+    def __create_object_arrays_from_script(self):
+
 
 
         return 1
 
-def create_script_object_from_name_and_file(script_text_file,file_name):
-
-    # Clean file name
-    cleand_name = file_name.replace("-", " ").replace(".txt", "")
-
-    # If last word in file is the move it to front
-    test_name = cleand_name.split()[-1].lower()
-    if (test_name == "the"):
-        cleand_name = cleand_name.replace(", The","").replace(", the","")
-        cleand_name = "The " + cleand_name
-
-    # Use imdb call to serach for meta date based on movie name
-    meta_data = imdb_data_call(cleand_name)
-
-    # Create script object
-    script_object = Script(script_text_file,meta_data.get("Title"),meta_data.get("imdbID"),meta_data)
-
-
-    print(meta_data)
 
 if __name__ == '__main__':
     with open("../Data/scripts_text/Abyss,-The.txt") as file:
         text_file = file.read()
 
-    create_script_object_from_name_and_file(text_file,"Abyss,-The.txt")
+    test_script = Script(text_file,"Abyss,-The.txt")
+
+    print(test_script.speech_object_array)
