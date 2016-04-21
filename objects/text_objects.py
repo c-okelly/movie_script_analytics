c9d2597ad5278bbef6e785723305e03dd7ca3ad2
cokelly <okellyconor@gmail.com>
2016-04-21 11:11:54 +0100

    Finished version 1 of text objects

--- objects/text_objects.py
+++ objects/text_objects.py
@@ -5,6 +5,91 @@ from nltk import tokenize
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import re
 
+class Speech:
+
+    def __init__(self,text,count):
+
+        self.text = text
+        self.count = count
+        # Set together
+        self.character = self.__return_character_name()
+        self.multiple_characters = self.__check_multiple_characters()
+        # Sentiment analysis averaged for section and returned.
+        self.sentimnet = self.__sentiment_analytis_text()
+        self.no_words = self.__return_no_words()
+        # Cleaned text version without character name
+        self.cleaned_text = self.__cleaned_text()
+
+    def __repr__(self):
+        return "Speech object for character " + self.character + " from " + str(self.count) + "% through the script."
+
+    def __return_character_name(self):
+
+        text = self.text
+
+        # Regex to pull out character name
+        try:
+            character_name = re.search("\s\w{2,}(/\w{2,}){0,}\s",text).group().strip()
+        except:
+            character_name = "Unknown"
+            print("Character name could not be found from text " + text)
+            pass
+
+        return character_name
+
+    def __check_multiple_characters(self):
+
+        # Test for multiple character names in same line
+        if (re.search("/",self.character)):
+            multiple_characters = 1
+        else:
+            multiple_characters = 0
+
+        return multiple_characters
+
+    def __sentiment_analytis_text(self):
+
+        text = self.text
+        # Strip character name from string
+        text = text.replace(self.character,"")
+
+        token_text = tokenize.sent_tokenize(text)
+        sid = SentimentIntensityAnalyzer()
+
+        over_all_sentimnet = 0
+        count = 0
+
+        for sentence in token_text:
+            score = sid.polarity_scores(sentence)
+            # Create over all sentiment score
+            over_all_sentimnet += score.get("compound")
+
+            # If sentence is not neuteral add to sentence count for average
+            if (score.get("compound") >  0.1):
+                count += 1
+
+        # Calculate average sentiment
+        if count > 0:
+            average_sentiment = over_all_sentimnet/count
+        else:
+            average_sentiment = over_all_sentimnet
+
+        return average_sentiment
+
+    def __return_no_words(self):
+
+        text = self.text
+        count = len(re.findall("\w+",text))
+
+        return count - 1
+
+    def __cleaned_text(self):
+
+        text = self.text
+        cleaned_text = text.replace(self.character,"")
+
+        return cleaned_text
+
 class Discription:
 
     def __init__(self,text,count):
@@ -12,65 +97,37 @@ class Discription:
         self.text = text
         self.count = count
 
-class Speech:
-
-    def __init__(self,text,count):
-
-        self.text = text
-        self.count = count
-        self.character = self.return_character_name()
-        self.sentimnet = self.sentiment_analytis_text()
-        self.no_words = self.return_no_words()
-
-    def return_character_name(self):
-
-        text = self.text
-
-        character_name = text.split("\n")[0]
-
-        character_name = character_name.strip()
-
-        return character_name
-
-    def sentiment_analytis_text(self):
-
-        text = self.text
-
-        token_text = tokenize.sent_tokenize(text)
-        sid = SentimentIntensityAnalyzer()
-
-        over_all_sentimnet = 0
-        for sentence in token_text:
-            # print(sentence)
-            score = sid.polarity_scores(sentence)
-            # print(score)
-            over_all_sentimnet += score.get("compound")
-        # print(over_all_sentimnet/10)
-
-    def return_no_words(self):
-
-        text = self.text
-        count = len(re.findall("\w+",text))
-
-        return count
-
-
 class Scene_change:
 
     def __init__(self,text,count):
 
         self.text = text
-        self.count = text
+        self.count = count
+        self.scene_change_to_outside = self.__scence_change_type()
 
+    def __repr__(self):
+        return "Sceane change at " + str(self.count) + "% through the script"
+
+    def __scence_change_type(self):
+
+        # regex to search for secene change
+        if (re.search("EXT.",self.text)):
+            change = 1
+        elif (re.search("INT.",self.text)):
+            change = 0
+        else:
+            change = None
+
+        return change
 
 
 
 if __name__ == '__main__':
 
-    text_ob = Speech("""                              WENDY
+    text_ob = Speech("""                              WENDY/James/John
                     Congratulations, Mike. You deserve
                     it. You're like a totally amazing
                     salesman.
                         """,0.5)
 
-    print(text_ob.no_words)
+    print(text_ob.multiple_characters)
