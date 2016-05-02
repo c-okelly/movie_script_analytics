3443dde6979412d3b92f8df8862605640cda37fd
cokelly <okellyconor@gmail.com>
2016-05-02 20:44:40 +0100

    Extended funationality for full cast

--- info_requests/extra_character_info_for_movie_dict.py
+++ info_requests/extra_character_info_for_movie_dict.py
@@ -14,12 +14,13 @@ import re
 def scrape_and_format_page_info(imdb_code):
 
     # Create request url
-    request_url = "http://www.imdb.com/title/" + imdb_code + "/"
+    request_url = "http://www.imdb.com/title/" + imdb_code + "/fullcredits?ref_=tt_cl_sm#cast"
 
 
     # Execute request and catch and errors
     # try:
     html_page = request.urlopen(request_url).read()
+    print("Page recieved")
 
     cleaned_html = BeautifulSoup(html_page,"html.parser")
     cast_section = cleaned_html.find("table", {"class": "cast_list"}) # Find correct div and return all info including html
@@ -31,38 +32,42 @@ def scrape_and_format_page_info(imdb_code):
     # Cycle through each row. Extra data and add dic to main character dict. Skip first row.
     for i in range(1,len(only_rows)):
         row = only_rows[i]
+        # Break loop when table row get down to uncredited cast members
+        if len(row) == 1:
+            break
 
-        # Find all individual cells in current row
-        cells = row.findAll("td")
+        else:
+            # Find all individual cells in current row
+            cells = row.findAll("td")
 
-        # Skip cell 0 only image and get actor link and name
-        actor_link_code = re.search("(nm\d+)",str(cells[1])).group().replace("/","")
-        actor_name = cells[1].getText().strip().replace("\n","")
-        # print(actor_link_code,actor_name)
-
-
-        # Skip cell 2 only ... and find character information. Possibly multiple.
-        character_link = re.findall("(ch\d+)",str(cells[3]))
-        character_name = cells[3].getText().replace("\n","").split("/") # Remove html, strip, replace \n split \n
+            # Skip cell 0 only image and get actor link and name
+            actor_link_code = re.search("(nm\d+)",str(cells[1])).group().replace("/","")
+            actor_name = cells[1].getText().strip().replace("\n","")
+            # print(actor_link_code,actor_name)
 
 
-        # Create new character dict
-        single_character = {"actor_name":actor_name,"actor_link_code":actor_link_code}
+            # Skip cell 2 only ... and find character information. Possibly multiple.
+            character_link = re.findall("(ch\d+)",str(cells[3]))
+            character_name = cells[3].getText().replace("\n","").split("/") # Remove html, strip, replace \n split \n
 
-        # Add character name and links for each character
-        count = 0
-        for i in character_name:
-            char_name = "character_name_" + str(count)
-            single_character[char_name] = character_name[count].replace("(voice)","").strip() # Remove voice information
-            char_link = "character_link_" + str(count)
-            # Try as some character my not have links
-            try:
-                single_character[char_link] = character_link[count]
-            except:
-                single_character[char_link] = ""
-            count += 1
 
-        # print(single_character.get("actor_name"))
+            # Create new character dict
+            single_character = {"actor_name":actor_name,"actor_link_code":actor_link_code}
+
+            # Add character name and links for each character
+            count = 0
+            for i in character_name:
+                char_name = "character_name_" + str(count)
+                single_character[char_name] = character_name[count].replace("(voice)","").strip() # Remove voice information
+                char_link = "character_link_" + str(count)
+                # Try as some character my not have links
+                try:
+                    single_character[char_link] = character_link[count]
+                except:
+                    single_character[char_link] = ""
+                count += 1
+
+            print(single_character.get("actor_name"))
 
     return character_dict
 
